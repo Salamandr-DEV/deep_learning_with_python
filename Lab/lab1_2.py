798b597b89b99b829a64fc37a5d71dbf15dce739
Salamandr-DEV <60989946+Salamandr-DEV@users.noreply.github.com>
2020-03-01 16:39:45 +0300

    Add files via upload

---
+++ Lab/lab1_2.py
@@ -0,0 +1,16 @@
+def myFib(num):
+    first = 1
+    second = 1
+    result = 0
+    for i in range(3,num+1):
+        result = first + second
+        second = first
+        first = result
+    if num == 1:
+        return first
+    if num == 2:
+        return second
+    else:
+        return result
+n = 8
+print(myFib(n))
